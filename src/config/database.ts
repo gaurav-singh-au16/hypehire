05470515ef8cb370d8604872dde52b91e70c3b4f
Gaurav Singh <gauravsingh.info@gmail.com>
2023-08-07 16:41:20 +0530

    fixes

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,15 +2,17 @@ import { Sequelize } from 'sequelize-typescript'
 import dotenv from 'dotenv';
 dotenv.config()
 
-const db = new Sequelize({
-  database: process.env.DB_NAME,
-  dialect: 'mysql',
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  storage: ':memory:',
-  logging: false,
-  host: process.env.DB_HOST
+// const db = new Sequelize({
+//   ssl: false,
+//   database: process.env.DB_NAME,
+//   dialect: 'postgres',
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   storage: ':memory:',
+//   logging: false,
+//   host: process.env.DB_HOST 
 
-})
+// })
+const db = new Sequelize(process.env.POSTGRES_URL+ "?sslmode=require")
 
 export default db;
